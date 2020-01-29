98eee767c80ecb10f91264ac47525b65c81e85fb
CordelleW <35435787+CordelleW@users.noreply.github.com>
2020-01-29 17:34:15 -0500

    Update movies.java

--- src/movies.java
+++ src/movies.java
@@ -13,6 +13,7 @@ public class movies {
         System.out.println("ABC 123 ABC");
         System.out.println("Gitbuttest");
         System.out.println("Gitbuttes12321312");
+        System.out.println("part time shiet");
         
     }
 }
